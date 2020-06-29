f9d40a05b603c9aebca0ba2f37fa94e2f33f7f53
Younahh <60348335+Younahh@users.noreply.github.com>
2020-06-29 17:34:38 +0100

    Update Translater.java

--- src/main/Translater.java
+++ src/main/Translater.java
@@ -14,9 +14,9 @@ import com.ibm.watson.language_translator.v3.LanguageTranslator;
 import com.ibm.watson.language_translator.v3.model.TranslateOptions;
 import com.ibm.watson.language_translator.v3.model.TranslationResult;
 
-import com.cybozu.labs.langdetect.Detector;
+/*import com.cybozu.labs.langdetect.Detector;
 import com.cybozu.labs.langdetect.DetectorFactory;
-import com.cybozu.labs.langdetect.LangDetectException;
+import com.cybozu.labs.langdetect.LangDetectException;*/
 
 @WebServlet("/Translater")
 @MultipartConfig
@@ -29,13 +29,13 @@ public class Translater extends HttpServlet {
 	public Translater() {
         super();
         languageTranslator.setServiceUrl("https://api.us-south.language-translator.watson.cloud.ibm.com/instances/475d396d-751e-44c0-899d-2fb130b48923");
-        String profileDirectory = "C:\\Users\\tacey\\eclipse-workspace\\Translater_Project\\profiles";
+        /*String profileDirectory = "C:\\Users\\tacey\\eclipse-workspace\\Translater_Project\\profiles";
         
         try {
     		DetectorFactory.loadProfile(profileDirectory);
     	} catch (LangDetectException e1) {
     		e1.printStackTrace();
-    	}
+    	}*/
     }
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -44,12 +44,12 @@ public class Translater extends HttpServlet {
 		String LangueOutput=request.getParameter("out");
 		
 		
-		System.out.println(textInput);	
+		/*System.out.println(textInput);	
 		Detector detector;
 		try {   
 			detector = DetectorFactory.create();
 			detector.append(textInput);
-			System.out.println("xxx " + detector.detect());	
+			System.out.println("xxx " + detector.detect());	*/
 		
 		String xInput;
 		
@@ -64,12 +64,12 @@ public class Translater extends HttpServlet {
 		
 		case "espagnol":
 			xInput=Language.ES;
-			System.out.println("lang es " + xInput);
+			//System.out.println("lang es " + xInput);
 			break;
 			
 		default :			
-			xInput=detector.detect();
-			System.out.println("lang " + xInput);
+			xInput=Language.EN;
+			//System.out.println("lang " + xInput);
 		}
 		
 		String xOutput;
@@ -98,12 +98,12 @@ public class Translater extends HttpServlet {
 		TranslateOptions translateOptions = new TranslateOptions.Builder().addText(textInput).source(xInput).target(xOutput).build();
 		TranslationResult translationResult = languageTranslator.translate(translateOptions).execute().getResult();
 		
-		System.out.println("xx" + translationResult.getTranslations().get(0).getTranslation());
+		//System.out.println("xx" + translationResult.getTranslations().get(0).getTranslation());
 		response.getWriter().print(translationResult.getTranslations().get(0).getTranslation());
 
-		} catch (LangDetectException e) {
+		} /*catch (LangDetectException e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 	
 }
